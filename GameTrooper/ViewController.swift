1362530aff1c33dbeb29a39b71de7020edc85f95
ray <raykrishardi@gmail.com>
2017-04-16 15:13:34 +1000

    Initial Commit

---
+++ GameTrooper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameTrooper
+//
+//  Created by Ray Krishardi Layadi on 16/4/17.
+//  Copyright © 2017 Ray Krishardi Layadi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
